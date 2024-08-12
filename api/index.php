8d57a8d25f0d2ad9117e67688607ab11656ebe68
HUNG <hung060101@gmail.com>
2024-08-13 03:42:37 +0700

    update

--- api/index.php
+++ api/index.php
@@ -57,7 +57,7 @@
   </div>
   <button id="scrollToTopBtn" style="display: none;">Back To Top</button>
 
-  <script src="../js/main.js"></script>
+  <script src="../js/main.min.js"></script>
 </body>
 
 </html>
